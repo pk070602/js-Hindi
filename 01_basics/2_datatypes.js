4c280c6eefd1ce117655f9ddbd97cf1adf0d11cd
pk070602 <163675893+pk070602@users.noreply.github.com>
2024-09-18 15:57:57 +0000

    datatypes

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"        // treat all Js code as newer version //
+
+// alert(3+3)   //we  are using nodejs not browser
+
+//code readability should be high
+console.log(3+3);  console.log("hitesh")      //ek line mai do code mt likho  (yha per semicolon lgana jaruri hai)
+
+//data types
+// 1 primitive data types
+
+let name="priyanshu";
+let age=19;
+let islodIn=false;
+
+// numbers=2^53
+// bigint     // ye big companies mai use krte hai ya bdi website per
+// string=> "" or ''   // it can be use in single quote or double quote
+// boolean => true/false
+// null  // it is basically a standalone value   or it indicates we have give to a empty value
+// undefined
+// symbol    // ye jyada tr react mai use krte hai  or kisi component ko unique btane k liye hum ye use krte hai
+
+
+// object
+console.log(typeof "string");   // type of  basically hume data k baare mai bta deta hai
+
+// *********************************************************************************************
+console.log(typeof  null);   // ye show krega object
+console.log(typeof undefined)   //ye show undefined krega kuki undefind apne aaap mai ek type hai
