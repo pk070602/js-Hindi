2ce4678995976dc6cff5367fbfb8510600e2da98
pk070602 <163675893+pk070602@users.noreply.github.com>
2024-09-19 07:13:01 +0000

    operations

--- 01_basics/3_conversionoperations.js
+++ 01_basics/3_conversionoperations.js
@@ -49,3 +49,47 @@ console.log(typeof numm);
 let convertIntoStr=String(numm);
 console.log(typeof convertIntoStr);
 console.log(convertIntoStr);
+
+
+
+/*********************************** Operators **********************************/
+//ex-1
+let value=3;
+let negVal=-value;
+console.log(negVal);
+
+//ex-2 (basics operations)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);        //2 ki power mai aa jata hai two star lgane per
+// console.log(3/8);
+// console.log(8%5);
+
+
+
+let str1="Hello";
+let str2=" Priyanshu";
+console.log(str1+str2);   //dono string add ho jayegi
+
+//some typical cases
+console.log(1+"3");
+console.log("1"+3);  //output=>13
+console.log("1"+3+2);  //starting agr string se ho tho wo baakiyo ko bhi string ki trh hi treat krega
+console.log(2+3+"1");    //output=>51
+
+console.log(4*3/2%2) ; //aisa code mt likho khrab tarika hai code review hi nhi hoga
+console.log(+true);  //output 1 hoga 
+
+
+let nu1 ,nu2,nu3;
+nu1=nu2=nu3=2+2;  // ye tarika bhi sahi nhi hai khrab lgta hai code
+
+//increment or decrement (prefixincrement and postfixincrement)
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+
+//https://tc39.es/ecma262/multipage/abstract-operations
